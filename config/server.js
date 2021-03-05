bcbbd2e3fab47739b359cdcb604b4a821cccc204
[abdhmd] <[sirhmd@outlook.com]>
2021-03-05 15:22:26 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5c8df95cbb82db4dc30949b437450916'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '253d9230-a79f-4056-bebb-f13f2eb5e22b'
+};
