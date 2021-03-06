c90d56fc67a251eb394d1ede444f7d2302918501
[abdhmd] <[sirhmd@outlook.com]>
2021-03-06 15:05:17 +0100

    port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1330),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5c8df95cbb82db4dc30949b437450916'),
